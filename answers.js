f3ed109173e7b6df104b8d790d69e8c2f9713a4f
Takanari Sasaki <takanari.s@hotmail.com>
2016-07-05 18:11:22 +0000

    Question 3: Character at position represented by a number

--- answers.js
+++ answers.js
@@ -1,5 +1,6 @@
 //Workshop 1 answers
 
+//Find the first letter of the String
 function firstChar(string) {
     return string.substring(0,1);
 }
@@ -8,12 +9,21 @@ var firstLetter = firstChar("hello");
 console.log(firstLetter);
 
 
-function lastChar (string) {
-    var lengthOfString = string.length;
-    return string.substring(lengthOfString - 1, lengthOfString);
+//Find the last letter of the String
+function lastChar (inputString) {
+    var lengthOfString = inputString.length;
+    return inputString.substring(lengthOfString - 1, lengthOfString);
     
 }
 
 var lastLetter = lastChar("hello");
 console.log(lastLetter);
 
+
+//Find the character at the position represented by the number
+function charAtPositionNumber(inputString, num) {
+    return inputString.charAt(num);
+}
+
+var letterAtPositionNumber = charAtPositionNumber("hello", 2);
+console.log(letterAtPositionNumber);
