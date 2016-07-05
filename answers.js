aac269b09b2aeba9e82f13ad43b7869b677c97d0
Takanari Sasaki <takanari.s@hotmail.com>
2016-07-05 18:02:20 +0000

    Question 2: Last Character

--- answers.js
+++ answers.js
@@ -6,3 +6,14 @@ function firstChar(string) {
 
 var firstLetter = firstChar("hello");
 console.log(firstLetter);
+
+
+function lastChar (string) {
+    var lengthOfString = string.length;
+    return string.substring(lengthOfString - 1, lengthOfString);
+    
+}
+
+var lastLetter = lastChar("hello");
+console.log(lastLetter);
+
